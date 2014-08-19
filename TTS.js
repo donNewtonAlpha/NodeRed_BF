78e830b27b63f4142840bd19ea3bc031c9f081ac
Don Newton <donNewtonAlpha@gmail.com>
2014-08-19 14:36:11 -0400

    fixed module.exports

--- TTS.js
+++ TTS.js
@@ -1,5 +1,5 @@
-function TextToSpeechNode(config){
-   module.exports = function(RED) { 
+module.exports = function(RED) { 
+   function TextToSpeechNode(config){
       var http=require('https');
       var fs=require('fs');
       this.context = {global:RED.settings.functionGlobalContext || {}};
@@ -11,12 +11,11 @@ function TextToSpeechNode(config){
       this.blackflag=config.blackflag;
       var wav=new Buffer(100000);
 
-
-       this.on('input', function(msg) {
-             if(msg.payload.text)
-                this.text=msg.payload.text;
-             if(msg.tmp_dir)
-                this.tmp_dir=msg.tmp_dir;
+      this.on('input', function(msg) {
+            if(msg.payload.text)
+            this.text=msg.payload.text;
+            if(msg.tmp_dir)
+               this.tmp_dir=msg.tmp_dir;
              console.log("tmp_dir  "+this.tmp_dir);
              console.log("msg.text "+msg.text);
              console.log("this.text "+this.text);
@@ -24,43 +23,43 @@ function TextToSpeechNode(config){
              var headers={ 'Content-Type': 'text/plain', 'Accept': 'audio/x-wav','Content-Length':this.text.length };
              headers.Authorization="Bearer "+this.context.global[this.blackflag];
              console.log(headers);
-              var options = {
+             var options = {
                  hostname: 'api.att.com',
                  port: 443,
                  path: '/speech/v3/textToSpeech',
                  method: 'POST'
               };
 
-                 var file_name="";
-                 file_name+=this.tmp_dir;
-                 var date=new Date().getTime();
-                 var short_name="";
-                 short_name+=date.toString()+".wav";
-                 file_name+="/"+short_name;
-                
-              options.headers=headers;
-              var req=http.request(options,function(res){
-              var i=0;
-              res.on('data',function(chunk){
-                    fs.appendFileSync(file_name,chunk);
-                    console.log("got a chunk");
-                    if(i==0){ 
-                       msg.payload.audio_file=short_name;
-                       node.send(msg);
-                       i=1;
-                    }
+                    var file_name="";
+                    file_name+=this.tmp_dir;
+                    var date=new Date().getTime();
+                    var short_name="";
+                    short_name+=date.toString()+".wav";
+                    file_name+="/"+short_name;
+                   
+                 options.headers=headers;
+                 var req=http.request(options,function(res){
+                 var i=0;
+                 res.on('data',function(chunk){
+                       fs.appendFileSync(file_name,chunk);
+                       console.log("got a chunk");
+                       if(i==0){ 
+                          msg.payload.audio_file=short_name;
+                          node.send(msg);
+                          i=1;
+                       }
+                    });
                  });
-              });
-              req.on('error',function(e){
-                 console.log('problem with request: ' +e.message);
-              });
-              console.log(this.text);
-              req.write(this.text);
-              req.end();
+                 req.on('error',function(e){
+                    console.log('problem with request: ' +e.message);
+                 });
+                 console.log(this.text);
+                 req.write(this.text);
+                 req.end();
 
 
-                     // do something with 'msg' 
-             });
-   }
-   RED.nodes.registerType("T SPEECH",TextToSpeechNode);
+                        // do something with 'msg' 
+                });
+      }
+      RED.nodes.registerType("T SPEECH",TextToSpeechNode);
 }
